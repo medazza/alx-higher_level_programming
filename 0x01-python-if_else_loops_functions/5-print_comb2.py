6b081176eeaf54f26be49791820025e501414d44
medazza <midazza27@gmail.com>
2023-10-03 12:45:01 -0700

    task 5 of 0x01-python-if_else_loops_functions update3

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(100):
     if n == 99:
-        print("{}.format(n)")
+        print("{}".format(n))
     else:
-        print("{:02}.format(n)", end=", ")
+        print("{:02}".format(n), end=", ")
