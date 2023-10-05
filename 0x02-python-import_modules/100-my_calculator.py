8ee8be30d62a749718d1d416bdfa43901e242c15
medazza <midazza27@gmail.com>
2023-10-05 14:06:11 -0700

    task adv 1 of 0x02-python-import_modules another update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div 
     import sys
 
-    if len(sys.argv)  != 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
