33bbf72efe614919913f1eba10c74917fac5dc5b
medazza <midazza27@gmail.com>
2023-10-05 13:52:32 -0700

    task adv 3 of 0x02-python-import_modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    """python bytecode."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for k in range(4, 6):
+            c = add(c, k)
+        return (c)
+
+    else:
+        return(sub(a, b))
