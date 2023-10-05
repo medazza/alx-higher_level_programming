22b903128c7341d29076b5156a4e3457bdb17831
medazza <midazza27@gmail.com>
2023-10-05 14:08:30 -0700

    task adv 1 of 0x02-python-import_modules another update2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     """Handle some basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
+    from calculator_1 import add, sub, mul, div
     import sys
 
     if len(sys.argv) != 4:
