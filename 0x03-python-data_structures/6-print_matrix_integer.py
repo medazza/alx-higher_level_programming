66a9d2b51bfec2861ffc5a84f5d37bc242859474
medazza <midazza27@gmail.com>
2023-10-07 23:03:08 -0700

    task adv 6 of 0x03-python-data_structures up date

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     """ prints a matrix of integers. """
+    if not matrix:
+        return None
     for r in matrix:
-        for c in row:
+        for c in r:
             print("{:d}".format(c), end=" " if c != r[-1] else "")
         print()
