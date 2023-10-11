1f7ab3fc7220890e7e47935d3da49f7f3ba02b36
medazza <midazza27@gmail.com>
2023-10-11 13:32:49 -0700

    task 12 of 0x04-python-more_data_structures update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
-
-def to_subtract(list_num):
-    max_val = max(list_num)
-    return (max_val - sum(n for n in list_num if max_val > n))
-
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return (0)
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
     num = 0
-    last_rom = 0
-    list_num = [0]
-    for ch in roman_string:
-        if ch in rom_n:
-            ch_value = rom_n[ch]
-            if ch_value <= last_rom:
-                num += to_subtract(list_num)
-                list_num = [ch_value]
-            else:
-                list_num.append(ch_value)
-            last_rom = ch_value
-    num += to_subtract(list_num)
-    return (num)
+    for ch in reversed(roman_string):
+        num = digits[ch]
+        total += num if total < num * 5 else -num
+    return (total)
