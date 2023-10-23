4d28f87f4b7df06c5bba23b2ddd87f457693998b
medazza <midazza27@gmail.com>
2023-10-23 13:38:17 -0700

    task 100 of 0x05-python-exceptions update 4

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,10 @@
 import sys
 
 def safe_print_integer_err(value):
+    is_intgr = True
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return False
-    else:
-        return True
+        is_intgr = False
+    return (is_intgr)
