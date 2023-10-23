d15eac411b571b32bd341ec1c87db1e64f63425a
medazza <midazza27@gmail.com>
2023-10-23 13:41:27 -0700

    task 101 of 0x05-python-exceptions update

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,4 +6,4 @@ def safe_function(fct, *args):
         return (res)
     except Exception as er:
         print("Exception: {}".format(er), file=sys.stderr)
-        return (None)
+        return None
