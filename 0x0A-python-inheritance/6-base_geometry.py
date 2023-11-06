1f74e364db4ad6d5400ca7642e35ece881b2a815
medazza <midazza27@gmail.com>
2023-11-06 08:36:34 -0800

    task 6 of 0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module contains one class"""
+
+
+class BaseGeometry:
+    """ BaseGeometry Class """
+
+    def area(self):
+        """ Not Implemented Yet."""
+
+        raise Exception("area() is not implemented")
