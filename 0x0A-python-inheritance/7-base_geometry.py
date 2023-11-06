2c2dd6b88f107ee5a4ffd04e6359fc803f442200
medazza <midazza27@gmail.com>
2023-11-06 08:41:26 -0800

    task 7 of 0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module of basegeometry class"""
+
+
+class BaseGeometry:
+    """ baseGeometry Class """
+
+    def area(self):
+        """ Not Yet Implemented """
+    
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates Value name is assumed to be a string"""
+
+        if type(value) != int:
+            mesg = "{:s} must be an integer".format(name)
+            raise TypeError(mesg)
+        if value <= 0:
+            mesg = "{:s} must be greater than 0".format(name)
+            raise ValueError(mesg)
