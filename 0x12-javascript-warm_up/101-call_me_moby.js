28faa99acf0cff0cf8f2939312b8218df6dff2ee
medazza <midazza27@gmail.com>
2024-01-08 13:18:58 -0800

    task 15 update

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
 module.exports = {
-  callMeMoby: function (x, func) {
+  callMeMoby: function (x, theFunction) {
     for (let ind = 0; ind < x; ind++) {
-      funct();
+      theFunction();
     }
   }
 };
