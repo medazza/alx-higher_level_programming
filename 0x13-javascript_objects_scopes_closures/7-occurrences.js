ba85a62b98d10f55f5861e3801f1ded29dc594bf
medazza <midazza27@gmail.com>
2024-01-10 12:39:11 -0800

    task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let ind = 0; ind < list.length; ind++) {
+    if (list[ind] === searchElement) {
+	    count++;
+    }
+  }
+  return count;
+};
