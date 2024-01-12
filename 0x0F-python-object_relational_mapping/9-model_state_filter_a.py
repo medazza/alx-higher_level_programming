d51a0b0b2c7c43f5ab4c743e1201b8f2a41c996a
medazza <midazza27@gmail.com>
2024-01-12 11:52:26 -0800

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        query = select(State).order_by(State.id.asc()) \
+            .where(State.name.like("%a%"))
+        states = connection.execute(query)
+        for state in states:
+            print(f"{state.id}: {state.name}")
+
+    engine.dispose()
