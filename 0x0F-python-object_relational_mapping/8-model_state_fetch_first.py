96e06cefd8c0a59f5006c2b526a238edab2151ec
medazza <midazza27@gmail.com>
2024-01-12 11:49:43 -0800

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        query = select(State).order_by(State.id.asc())
+        state = connection.execute(query).fetchone()
+        if state:
+            print(f"{state.id}: {state.name}")
+        else:
+            print("Nothing")
+
+    engine.dispose()
