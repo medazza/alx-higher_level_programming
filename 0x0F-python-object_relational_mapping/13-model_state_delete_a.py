0682cd9881b03c04362312685da087b2bf5e3f0a
medazza <midazza27@gmail.com>
2024-01-12 12:13:52 -0800

    task 12-14

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, select, insert
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    deleteStates = session.query(State).filter(State.name.like(f'%a%')).all()
+    if deleteStates:
+        for state in deleteStates:
+            session.delete(state)
+        session.commit()
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""model state"""
+from sqlalchemy import create_engine
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(City, State).\
+        join(State, State.id == City.state_id).all()
+    if result:
+        for city, state in result:
+            print(f"{state.name}: ({city.id}) {city.name}")
+    session.close()
