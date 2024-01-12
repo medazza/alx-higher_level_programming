daaae0756cdaf862eea58e3ebf99919be9cf6aa3
medazza <midazza27@gmail.com>
2024-01-12 12:03:07 -0800

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select, insert
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        transaction = connection.begin()
+        query = insert(State).values(name="Louisiana")
+        connection.execute(query)
+        query = select(State).where(State.name == "Louisiana")
+        state = connection.execute(query).fetchone()
+        if state:
+            print(state.id)
+        transaction.commit()
+
+    engine.dispose()
