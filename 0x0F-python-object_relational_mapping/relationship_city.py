c971c8a4beb4919487a1f5af813a2918b344d938
medazza <midazza27@gmail.com>
2024-01-13 12:03:36 -0800

    task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""model state"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+
+
+class City(Base):
+    """ the class class """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""model state"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ the class class """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade="all, delete", backref='state')
