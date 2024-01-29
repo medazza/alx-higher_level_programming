e5b2bc89bcde12b892e5d293bbd90797f328f56f
medazza <midazza27@gmail.com>
2024-01-29 11:52:46 -0800

    task 1 python ntwork_1 update 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-Displays the X-Request-Id header request to url.
+Displays the X-Request-Id header req to url
 usage:
     ./1-hbtn_header.py url
 """
+
 import urllib.request
 import sys
 
-url = urllib.request.Request(sys.argv[1])
+url = sys.argv[1]
+req = urllib.request.Request(url)
 with urllib.request.urlopen(url) as response:
     res = response.headers.get('X-Request-Id')
 
