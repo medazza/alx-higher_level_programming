d67831667fe916f847f3b2dac18c6a37da22af0d
medazza <midazza27@gmail.com>
2024-02-20 12:33:59 -0800

    task 3 web scraping

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});
